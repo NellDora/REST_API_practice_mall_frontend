6a02b3402645f88eaf214b5f25748529c7155948
NellDora <dorashin9471@gmail.com>
2024-02-20 23:53:13 +0900

    dynamic page settings

--- src/pages/todo/ReadPage.js
+++ src/pages/todo/ReadPage.js
@@ -1,14 +1,37 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { createSearchParams, useNavigate, useParams, useSearchParams} from 'react-router-dom';
 
 function ReadPage(props){
 
-    const obj = useParams()
+    const navigate = useNavigate()
 
-    console.log(obj)
+    const {tno} = useParams()
+
+    const [queryParams] = useSearchParams()
+
+    const page = queryParams.get('page') ? parseInt(queryParams.get('page')) : 1
+    const size = queryParams.get('size') ? parseInt(queryParams.get('size')) : 10
+
+    //createSearchOaram 함수를 사용하면 주소창 쿼리 물음표를 만들어줌
+    const queryStr = createSearchParams({page:page, size:size}).toString;
+
+    console.log(tno)
+
+    const moveToModify = (tno)=>{
+        navigate({
+            pathname:`/todo/modify/${tno}`,
+            search : queryStr
+        })
+
+    }
+
     return(
         <div className={'text-3xl'}>
-            Todo Read Page
+            Todo Read Page {tno}
+
+            <div>
+                <button onClick={()=>moveToModify(tno)}> Test Modify</button>
+            </div>
         </div>
     );
 }
